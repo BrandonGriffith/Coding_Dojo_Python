961b2fcfb5c51954ac45a9983c6cf4d658679733
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-01-14 11:09:12 -0500

    Updated

---
+++ python/Algo/algo_w1_d5.js
@@ -0,0 +1,53 @@
+function ConvertToCoinChange(input) {
+    var coins = [0, 0, 0, 0];
+
+    for ( input; input >= .25; input -= .25 ) {
+        coins[0] = coins[0] + 1;
+        input = input.toFixed(2);
+    }
+
+    for ( input; input >= .10; input -= .10 ) {
+        coins[1] = coins[1] + 1;
+        input = input.toFixed(2);
+    }
+
+    for ( input; input >= .05; input -= .05 ) {
+        coins[2] = coins[2] + 1 ;
+        input = input.toFixed(2);
+    }
+
+    for ( input; input > .00; input -= .01 ) {
+        coins[3] = coins[3] + 1;
+        input = input.toFixed(2);
+    }
+
+    var str = "";
+
+    if ( coins[0] >= 1 ) {
+        str = str + coins[0] + " quarters, ";
+    }
+    if ( coins[1] >= 1 ) {
+        str = str + coins[1] + " dimes, ";
+    }
+    if ( coins[2] >= 1 ) {
+        str = str + coins[2] + " nickels, ";
+    }
+    if ( coins[3] >= 1 ) {
+        str = str + coins[3] + " pennys";
+    }
+    return str;
+}
+
+console.log(ConvertToCoinChange(3.22));
+
+console.log(ConvertToCoinChange(3.21));
+
+console.log(ConvertToCoinChange(9.68));
+
+console.log(ConvertToCoinChange(0.03));
+
+console.log(ConvertToCoinChange(0.02));
+
+console.log(ConvertToCoinChange(0.01));
+
+console.log(ConvertToCoinChange(1.22));
