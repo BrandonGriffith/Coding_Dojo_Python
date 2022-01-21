b1a471a14c4c9e6105dab19e39181afabec905f6
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-01-21 00:31:05 -0500

    updated

--- python/flask_mysql/FullStack/users/users.py
+++ python/flask_mysql/FullStack/users/users.py
@@ -25,3 +25,19 @@ class User:
         VALUES (%(first_name)s,%(last_name)s,%(email)s,now(),now());"""
         result = connectToMySQL('users').query_db(query,data)
         return result
+
+    @classmethod
+    def get_one(cls,data):
+        query  = "SELECT * FROM users WHERE id = %(id)s";
+        result = connectToMySQL('users').query_db(query,data)
+        return cls(result[0])
+
+    @classmethod
+    def update(cls,data):
+        query = "UPDATE users SET first_name=%(first_name)s,last_name=%(last_name)s,email=%(email)s,updated_at=NOW() WHERE id = %(id)s;"
+        return connectToMySQL('users').query_db(query,data)
+
+    @classmethod
+    def destroy(cls,data):
+        query  = "DELETE FROM users WHERE id = %(id)s;"
+        return connectToMySQL('users').query_db(query,data)
