42c87ceab3eeb08f61e8b8792edce76c06fed558
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-02-02 16:08:40 -0500

    updated

--- python/python_flask_project/travel_marker/flask_app/models/wiki.py
+++ python/python_flask_project/travel_marker/flask_app/models/wiki.py
@@ -2,11 +2,11 @@ import wikipedia
 
 def wiki_summary(data):
     data1 = wikipedia.search(data, results=3)
+    if (data == "New York" or data == "new york"):
+            data1 = ["New York (state)"]
     try:
         data1 = wikipedia.summary(data1[0])
-    except (wikipedia.exceptions.PageError, wikipedia.exceptions.DisambiguationError):
-        if (data1[0] == "new york sity"):
-            data1 = ['',"New York (state)"]
+    except(wikipedia.exceptions.DisambiguationError,wikipedia.exceptions.PageError):
         data1 = wikipedia.summary(data1[1])
     return data1
 
